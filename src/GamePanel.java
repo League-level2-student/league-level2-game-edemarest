a8d455df755e9210683174e9efca19c03227bc7a
edemarest <Lunacat7!>
2020-02-11 16:29:18 -0800

    2/11/2020

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,18 +2,23 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GamePanel extends JPanel implements ActionListener{
+public class GamePanel extends JPanel implements ActionListener, KeyListener{
 	
 	Timer frameDraw;
 	private BufferedImage background;
+	public static BufferedImage image;
+	public static boolean needImage = true;
+	public static boolean gotImage = false;	
 	private int backgroundHeight = 0; //You need this if you are scrolling up-down
-	private int scrollSpeed = 5;
+	private int scrollSpeed = 50;
 	int HEIGHT = GameRunner.HEIGHT;
 	int WIDTH = GameRunner.WIDTH;
 	int y1 = 7200-HEIGHT;
@@ -23,7 +28,7 @@ public class GamePanel extends JPanel implements ActionListener{
     final int GAME = 1;
     final int END = 2;
     
-    int currentState = GAME;
+    int currentState = START;
 	
 	public GamePanel(int frameWidth, int frameHeight) {
 
@@ -56,11 +61,13 @@ public class GamePanel extends JPanel implements ActionListener{
 	
 	void drawStartState(Graphics g) {
 		g.setColor(Color.PINK);
-		g.fillRect(0, 0, GameRunner.WIDTH, GameRunner.HEIGHT);
+		    loadImage ("Start Screen.png");
 	}
 	
 	void drawGameState(Graphics g) {
-		 
+		g.drawImage(background,0, 0, WIDTH, HEIGHT, 0, y1, WIDTH, y2,
+				this);
+		
 	}
 	
 	void drawEndState(Graphics g)  {
@@ -70,10 +77,7 @@ public class GamePanel extends JPanel implements ActionListener{
 	
 	@Override
 	public void paintComponent(Graphics g){
-		//g.fillRect(10, 10, 100, 100);
-		g.drawImage(background,0, 0, WIDTH, HEIGHT, 0, y1, WIDTH, y2,
-				this);
-		//g.drawImage(background, 0, 0, null);
+
 		
 		//Drawing game states
 		if(currentState == START){
@@ -102,10 +106,70 @@ public class GamePanel extends JPanel implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		moveBackground();
-		repaint();
+		
+		if(currentState == GAME) {
+			moveBackground();
+			repaint();
+		}
+		
+		
+	}
+
+
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		System.out.println("Key pressed");
+		// TODO Auto-generated method stub
+		if(e.getKeyCode()==KeyEvent.VK_ENTER) {
+			System.out.println(currentState);
+
+			if(currentState == START) {
+				currentState = GAME;
+			}
+			else if(currentState == GAME) {
+				currentState = END;
+			}
+			else if (currentState == END) {
+				currentState = START;
+		    } 
+		}
+	}
+
+
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
+	
+	//method to load images
+		void loadImage(String imageFile) {
+			System.out.println("Loading image...");
+		    if (needImage) {
+		        try {
+		            image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
+			    gotImage = true;
+			    System.out.println("Got Image");
+		        } catch (Exception e) {
+		            
+		        }
+		        needImage = false;
+		    }
+		}
 	
 	
 	
